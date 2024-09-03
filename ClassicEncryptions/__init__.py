78b3e3534c801ad0996d18a754451343694002e1
RodrigoCF25 <rodrigocf88@hotmail.com>
2024-09-03 02:19:43 -0600

    Changes in init, not impoting things from TextLib

--- ClassicEncryptions/__init__.py
+++ ClassicEncryptions/__init__.py
@@ -4,6 +4,6 @@ from .PolyAlphabeticCipher import PolyAlphabeticCipher
 from .SubstitutionCipher import SubstitutionCipher
 from .PlayfairCipher import PlayfairCipher
 from .HillCipher import HillCipher
-from TextLib import ALPHABET,ALPHABET_LENGTH, NormalizeText
+
 
 __all__ = ["CaesarCipher","TranspositionCipher","PolyAlphabeticCipher","SubstitutionCipher","PlayfairCipher","HillCipher"]
