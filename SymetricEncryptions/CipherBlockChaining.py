d659713f8b89f6b1a8fdbfb968a15f95580ddd55
RodrigoCF25 <rodrigocf88@hotmail.com>
2024-09-06 17:59:53 -0600

    Created the initial abstraction of CipherBlockChaining symetric key cipher

---
+++ SymetricEncryptions/CipherBlockChaining.py
@@ -0,0 +1,28 @@
+from TextLib import PaddingText,TextToBinary,BinaryToText
+
+from BinaryOperations import XOR
+
+
+class CipherBlockChaining:
+
+    def __init__(self):
+        """This Cipher cant be executed concurrently, because it needs the previous cipher block to encrypt the next block"""
+        pass
+
+    def __EncryptBlock(self,block,key,previous_cipher_block):
+        xored_block = XOR(block,previous_cipher_block)
+        cipher_block = XOR(xored_block,key)
+        return cipher_block
+
+    def Encrypt():
+        pass
+
+
+    def __DecryptBlock(self,cipher_block,key,previous_cipher_block):
+        xored_block = XOR(cipher_block,key)
+        plain_block = XOR(xored_block,previous_cipher_block)
+        return plain_block
+
+    def Decrypt():
+        pass
+
